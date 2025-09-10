c5fb8d6f20a8ac5755b708e5b4c878c7884a4801
Timitunes <timiswift0@gmail,com>
2025-09-10 11:21:22 +0100

    implemented app ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
 import { connectDB } from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 
 dotenv.config();
 const app = express();
-
+const __dirname = path.resolve();
 app.use(express.json()); //allows us to accept json data in the req.body
 
 
@@ -13,6 +14,13 @@ app.use("/api/products", productRoutes);
 
 const port = process.env.PORT || 5000;
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "frontend/dist")));
+
+    app.get(/.*/, (req, res) =>{
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+    }) 
+}
 
 connectDB().then(
     app.listen(port, () => {
